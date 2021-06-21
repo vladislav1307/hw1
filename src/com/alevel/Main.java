dda4536b8c3db6d79fc6fab93cbc6568dbe0f080
Vlad <vladchudik1307@gmail.com>
2021-06-21 12:24:50 +0300

    hw 1

--- src/com/alevel/Main.java
+++ src/com/alevel/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("С права на лево");
+        System.out.println("Справа налево");
         int x = 347_693_485;
         while (x > 0) {
             int y = x % 10;
@@ -20,13 +20,30 @@ public class Main {
             }
             x= x/10;
         }
+        System.out.println("Слева направо");
+        int z = 347_693_485;
+        String str = String.valueOf(z);
+        for(int i = 0; i < str.length(); i++) {
+            int y = Character.getNumericValue(str.charAt(i));
+            if (y % 2 == 0 && y % 3 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (y % 2 == 0) {
+                System.out.println("Fizz");
+            } else if (y % 3 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(y);
+            }
+        }
+
+
 
 
         int a = 347_693_485;
         String bin = Integer.toBinaryString(a);
         int counter = 0;
         for (int i = 0; i < bin.length(); i++){
-            if (bin.charAt(i) == 1) {
+            if (bin.charAt(i) == '1') {
                 counter++;
             }
         }
@@ -34,5 +51,8 @@ public class Main {
         System.out.println(counter);
 
 
+
+
     }
+
 }
